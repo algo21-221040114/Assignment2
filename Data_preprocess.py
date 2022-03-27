411f26bfa20a8969faf4fe038ba07d4424f4c90f
algo21-221040114 <221040114@link.cuhk.edu.cn>
2022-03-27 11:28:43 +0800

    edit

--- Data_preprocess.py
+++ Data_preprocess.py
@@ -1,3 +1,12 @@
-import nltk
-stop_words = nltk.corpus.stopwords.words('english')
-print(stop_words)
+from GoogleNews import GoogleNews
+
+
+googlenews = GoogleNews()
+googlenews = GoogleNews(lang='en')
+googlenews = GoogleNews(start='02/28/2020', end='02/28/2020')
+googlenews = GoogleNews(encode='utf-8')
+googlenews.get_news('IBM')
+titles = googlenews.get_texts()
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Assignment2
+
+stop word list reference: https://github.com/stopwords-iso/stopwords-iso
+positive negative words list: https://github.com/abdulfatir/twitter-sentiment-analysis
